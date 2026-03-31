c27677765c05be9023136547c53f906892eb9285
Daniel Todd <dgtutv@gmail.com>
2026-03-30 18:55:36 -0700

    Completed vid 3

--- server.py
+++ server.py
@@ -44,3 +44,53 @@ class Server(threading.Thread):
                 
     def remove_connection(self, connection):
         self.connections.remove(connection)
+        
+class ServerSocket(threading.Thread):
+    def __init__(self, sc, sockname, server):
+        super().__init__()
+        self.sc = sc
+        self.sockname = sockname
+        self.server = server
+    
+    def run(self):
+       while True:
+           message = self.sc.recv(1024).decode("ascii")
+           
+           if message:
+               print(f"{self.sockname} says {message}")
+               self.server.broadcast(message, self.sockname)
+            
+           else:
+               print(f"{self.sockname} has closed the connection")
+               self.sc.close()
+               server.remove_connection(self)
+               return
+    
+    def send(self, message):
+        
+        self.sc.sendall(message.encode('ascii'))
+        
+    def exit(server):
+        while True:
+            ipt = input("")
+            if ipt == "q":
+                print("Closing all connections... ")
+                for connection in server.connections:
+                    connection.sc.close()
+                    
+                print("Shutting down the server... ")
+                os.exit(0)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Chatroom Server")
+    parser.add_argument("host", help="Interface the server listens at")
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060, help="TCP port(defualt 1060)")
+    
+    
+    args = parser.parse_args()
+    
+    # Create and start server thread
+    server = Server(args.host, args.p)
+    server.start()
+    
+    exit = threading.Thread(target=exit, args=(server,))
+    exit.start()
